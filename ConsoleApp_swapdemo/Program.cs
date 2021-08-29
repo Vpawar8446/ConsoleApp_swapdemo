7827d1fe63c7bd47234ef1206f4bcc2abc04f15b
niles <niles@LAPTOP-3K24CQ1R>
2021-08-29 22:56:14 +0530

    Add project files.

---
+++ ConsoleApp_swapdemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp_swapdemo
+{
+
+    class swap
+    {
+        public void Swapping(ref int x, ref int y)
+        {
+            int tempswap = x;
+            x = y;
+            y = tempswap;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter 1st number : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter 2nd number : ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Value of num1 and num2 before swapping : ");
+            Console.WriteLine("num1 = " + " " + num1);
+            Console.WriteLine("num2 = " + " " + num2);
+            swap obj = new swap();
+            obj.Swapping(ref num1, ref num2);
+            Console.WriteLine("Value of num1 and num2 after swapping : ");
+            Console.WriteLine("num1 = " + " " + num1);
+            Console.Write("num2 = " + " " + num2);
+            
+        }
+    }
+}
